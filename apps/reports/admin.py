ffb5757151136fdf8c8e4ec2fd34e6760492f672
Mamunur Rashid <rrseone@gmail.com>
2026-01-05 00:10:33 +0600

    update requirement.txt

--- apps/reports/admin.py
+++ apps/reports/admin.py
@@ -5,3 +5,5 @@ from apps.reports.models import Report
 @admin.register(Report)
 class ReportsAdmin(admin.ModelAdmin):
     list_display = ['location', 'status']
+    search_fields = ['location', 'description']
+    list_filter = ['status']
